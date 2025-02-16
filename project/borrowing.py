fce57b881b5965f78f3577d2c7c479e9089058e3
Rene Reid <rdreid01@students.cs.ubc.ca>
2025-02-16 13:53:49 -0800

    more updates to game

---
+++ project/borrowing.py
@@ -0,0 +1,20 @@
+def display_options():
+    print("1: Borrow from bank")
+    print("2: Borrow from contacts")
+    print("3: Borrow from loan shark")
+    print("4: Go back")
+
+
+def borrow_money():
+    display_options()
+    choice = input("Enter your choice (1-3): ")
+    if choice == '1':
+        print("choice 1")
+    elif choice == '2':
+        print("choice 2")
+    elif choice == '3':
+        print("choice 3")
+    elif choice == '4':
+        return
+    else:
+        print("Invalid choice. Please select a number between 1 and 7.")
--- project/game_start.py
+++ project/game_start.py
@@ -34,21 +34,46 @@ def environment_setup(difficulty):
     print("this may be deleted/ subsumed into the game running bit, prior to while loop")
 
 def display_options():
-    print("Borrow money")
-    print("Purchase")
-    print("Sell")
-    print("Socialize")
-    print("Seek advice")
-    print("Go to court")
+    print("1: Borrow money")
+    print("2: Purchase")
+    print("3: Sell")
+    print("4: Socialize")
+    print("5: Seek advice")
+    print("6: Go to court")
+    print("7: Commit crime")
+    print("8: Save game")
+    print("9: Exit game")
 
 def game_running(player, difficulty):
     turn = 0
     while True:
+        display_options()
+        choice = input("Enter your choice (1-7): ")
+        if choice == '1':
+            print("choice 1")
+        elif choice == '2':
+            print("choice 2")
+        elif choice == '3':
+            print("choice 3")
+        elif choice == '4':
+            print("choice 4")
+        elif choice == '5':
+            print("choice 5")
+        elif choice == '6':
+            print("choice 6")
+        elif choice == '7':
+            print("choice 7")
+        elif choice == '8':
+            print("choice 8")
+        elif choice == '9':
+            print("Exiting this game")
+            break
+        else:
+            print("Invalid choice. Please select a number between 1 and 7.")
         #TODO: fill this in with content
         if not deduct_col(player):
             #TODO: go into personal insolvency or bankruptcy proceeding -- this is kind of like a end-game gateway
-        
-        
+            personal_insolvency(player)
         turn += 1
         break
     print("this is the main game loop that keeps the current game running")
@@ -57,8 +82,13 @@ def game_running(player, difficulty):
 def deduct_col(player):
         if (player.cash >= player.col):
             player.cash -= player.col
+            if (player.cash <= player.col): 
+                print("you are almost illiquid")
             return True
         else:
             #TODO: should this be an exception? 
             print("you are illiquid!")
             return False
+
+def personal_insolvency(player):
+    print("you are now in an insolvency proceeding")
