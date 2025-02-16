a9e0645614fd404d0c1ee63b898ef5765ff136ce
Rene Reid <rdreid01@students.cs.ubc.ca>
2025-02-16 10:40:20 -0800

    game loop started

--- project/classes/person.py
+++ project/classes/person.py
@@ -11,6 +11,9 @@ class Person:
         self.cash = self.set_cash(difficulty)
         self.liabilities = self.set_liabilities(difficulty)
         self.col = self.set_col(difficulty)
+        self.credit_rating = 10
+        self.clout = 0
+        self.health = 0
         self.contacts = []
         self.skills = []
     
@@ -35,12 +38,16 @@ class Person:
     def set_liabilities(self, difficulty):
         val = (difficulty) * 25000
         liab = math.ceil(np.random.normal(val, val/3, 1))
+        # TODO: this should be refined to include an interest rate, or a type of liability category
         return liab
 
     def set_col(self, difficulty):
         val = (difficulty) * 1000
         col = math.ceil(np.random.normal(val, val/3, 1))
+        # TODO: this should be broken out into categories, which a player can modify through decision-making
         return col
+    
+    
     
     def set_periodic_income(self, difficulty):
         print("this will likely be through variety of different asset classes")
--- project/game_start.py
+++ project/game_start.py
@@ -4,7 +4,8 @@ def start_game():
     print("you are starting a new game")
     difficulty = int(input("input a difficulty between 1 and 10, with 1 being easiest: "))
     #TODO: validate that difficulty is a number betweeen 1 and 10 (inclusive)
-    player = game_setup(difficulty)
+    player = player_setup(difficulty)
+    environment = environment_setup(difficulty)
     game_running(player, difficulty)
     
     # skills set up
@@ -17,7 +18,7 @@ def start_game():
     # recurring costs
     # recurring incomes
 
-def game_setup(difficulty):
+def player_setup(difficulty):
     name = input("please input your player name: ")
     try:
         player = Person(name, difficulty)
@@ -27,8 +28,37 @@ def game_setup(difficulty):
         print(e)
     print("this is where the game is set up")
 
+def environment_setup(difficulty):
+    # TODO: in this function, all the different entities that the player will interact with are set up
+    # for example, the different mineral locations, NPC's, banks, insurance companies, etc. 
+    print("this may be deleted/ subsumed into the game running bit, prior to while loop")
+
+def display_options():
+    print("Borrow money")
+    print("Purchase")
+    print("Sell")
+    print("Socialize")
+    print("Seek advice")
+    print("Go to court")
+
 def game_running(player, difficulty):
+    turn = 0
     while True:
         #TODO: fill this in with content
+        if not deduct_col(player):
+            #TODO: go into personal insolvency or bankruptcy proceeding -- this is kind of like a end-game gateway
+        
+        
+        turn += 1
         break
     print("this is the main game loop that keeps the current game running")
+
+
+def deduct_col(player):
+        if (player.cash >= player.col):
+            player.cash -= player.col
+            return True
+        else:
+            #TODO: should this be an exception? 
+            print("you are illiquid!")
+            return False
